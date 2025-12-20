83ceb6955b3fb9b4f8c0e5f70304d2b7e0abe18f
Martin Nastoupil <xnastoup@fi.muni.cz>
2025-12-20 09:40:40 +0100

    modified next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js or next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/npm/:path*",
+        destination: "https://img.clerk.com/npm/:path*",
+      },
+      {
+        source: "/_clerk/ip",
+        destination: "https://headers.clerk.com",
+      },
+      {
+        source: "/clerk/:path*",
+        destination: "https://api.clerk.com/:path*",
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
